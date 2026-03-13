451ee567bfc7aad220484d38663ebd91779a923c
mikhailantonovdigital-dev <mikhailantonovdigital@gmail.com>
2026-03-13 14:51:58 +0300

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,12 @@
 from app.models.user import User
 from app.models.order import Order, OrderEvent
 from app.models.voice_input import VoiceInput
+from app.models.lyrics_version import LyricsVersion
 
-__all__ = ["User", "Order", "OrderEvent", "VoiceInput"]
+__all__ = [
+    "User",
+    "Order",
+    "OrderEvent",
+    "VoiceInput",
+    "LyricsVersion",
+]
