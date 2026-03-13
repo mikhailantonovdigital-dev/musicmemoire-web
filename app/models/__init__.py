8af888e40455ed5bd3303f6e1221ebfd201a76d9
mikhailantonovdigital-dev <mikhailantonovdigital@gmail.com>
2026-03-13 23:31:13 +0300

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,6 +4,7 @@ from app.models.voice_input import VoiceInput
 from app.models.lyrics_version import LyricsVersion
 from app.models.magic_login_token import MagicLoginToken
 from app.models.order_payment import OrderPayment
+from app.models.song_generation import SongGeneration
 
 __all__ = [
     "User",
@@ -13,4 +14,5 @@ __all__ = [
     "LyricsVersion",
     "MagicLoginToken",
     "OrderPayment",
+    "SongGeneration",
 ]
