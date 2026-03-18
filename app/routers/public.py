7a5025cc89dec2adb99a15116cb9cbb2aae1fca8
mikhailantonovdigital-dev <mikhailantonovdigital@gmail.com>
2026-03-18 14:46:12 +0300

    Update public.py

--- app/routers/public.py
+++ app/routers/public.py
@@ -5,6 +5,7 @@ from fastapi.templating import Jinja2Templates
 from app.core.config import settings
 
 templates = Jinja2Templates(directory="app/templates")
+templates.env.globals["settings"] = settings
 
 router = APIRouter()
 
